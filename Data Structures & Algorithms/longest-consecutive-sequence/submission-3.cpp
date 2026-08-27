b5b6d5049f3fea79345b0b05118c20d22a1b6e1e
Aboderin Logo <oluwadeclanrice41@gmail.com>
2026-08-27 11:44:34 +0100

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        int longest_streak = 0;
+
+        for (int num : num_set) {
+            if (num_set.find(num - 1) == num_set.end()) {
+                int current_num = num;
+                int current_streak = 1;
+
+                while (num_set.find(current_num + 1) != num_set.end()) {
+                    current_num += 1;
+                    current_streak += 1;
+                }
+
+                longest_streak = max(longest_streak, current_streak);
+            }
+        }
+
+        return longest_streak;
+    }
+};
