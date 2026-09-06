cacc20af98b8f09157c584fd5e60a388fcf2a5c1
Aboderin Logo <oluwadeclanrice41@gmail.com>
2026-09-06 22:36:43 +0100

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (int num : nums) count[num]++;
+
+        // Bucket sort: index = frequency, value = list of numbers with that frequency
+        vector<vector<int>> buckets(nums.size() + 1);
+        for (auto& pair : count) {
+            buckets[pair.second].push_back(pair.first);
+        }
+
+        vector<int> result;
+        for (int i = buckets.size() - 1; i >= 0 && result.size() < k; i--) {
+            for (int num : buckets[i]) {
+                result.push_back(num);
+                if (result.size() == k) break;
+            }
+        }
+        return result;
+    }
+};
