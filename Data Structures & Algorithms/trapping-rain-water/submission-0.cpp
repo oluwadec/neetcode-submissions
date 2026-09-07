394d0c18dcfb1ba1db825e9a52023c438829154a
Aboderin Logo <oluwadeclanrice41@gmail.com>
2026-09-07 15:39:39 +0100

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+
+        int left = 0, right = height.size() - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+        return water;
+    }
+};
