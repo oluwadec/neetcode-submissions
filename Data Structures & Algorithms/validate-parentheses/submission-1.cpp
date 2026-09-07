caa2d78e6005cf7d3e4565260c01c4ab6f2c7587
Aboderin Logo <oluwadeclanrice41@gmail.com>
2026-09-07 15:41:04 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        unordered_map<char, char> pairs = {{')', '('}, {']', '['}, {'}', '{'}};
+
+        for (char c : s) {
+            if (pairs.count(c)) {
+                if (stk.empty() || stk.top() != pairs[c]) return false;
+                stk.pop();
+            } else {
+                stk.push(c);
+            }
+        }
+        return stk.empty();
+    }
+};
